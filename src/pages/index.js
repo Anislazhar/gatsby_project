caf3b14bc8662fe564c0efc65b355c1aaaf4dabc
Anislazhar <anis.lazhar@web.de>
2021-06-14 16:30:20 +0200

    change

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ data }) {
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
-          <p>UX designer & web developer based in Manchester.</p>
+          <p>UX designer & web developer based in Berlin.</p>
           <Link className={btn} to="/projects">
             My Portfolio Projects
           </Link>
